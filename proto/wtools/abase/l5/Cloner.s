7117434cb315da964ececff0fef54a3bf759dc12
wandalen <wandalen.me@gmail.com>
2021-02-15 18:51:44 +0200

    .

--- proto/wtools/abase/l5/Cloner.s
+++ proto/wtools/abase/l5/Cloner.s
@@ -257,7 +257,7 @@ function _cloneBufferUp( src, it )
   if( it.copyingDegree >= 2 )
   {
     // debugger;
-    /* xxx : use _.bufferMake maybe? */
+    /* zzz : use _.bufferMake maybe? */
     it.dst = _._longClone( it.src );
   }
   else
